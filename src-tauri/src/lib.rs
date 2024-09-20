3678231cdcf70312b230f23cc8ddf026d30c69d9
Cuervolu <angel.cuervo187@gmail.com>
2024-09-19 22:37:15 -0300

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,66 @@
+use tauri::Manager;
+use tauri_plugin_log::fern::colors::ColoredLevelConfig;
+use tauri_plugin_log::RotationStrategy;
+use tauri_plugin_decorum::WebviewWindowExt;
+fn setup_logger(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    // create the log plugin as usual, but call split() instead of build()
+    let (tauri_plugin_log, _max_level, logger) = tauri_plugin_log::Builder::new()
+        .max_file_size(1024 * 1024 * 10) // 10 MB
+        .rotation_strategy(RotationStrategy::KeepAll) // keep all logs in the log directory
+        .level(log::LevelFilter::Info)
+        .with_colors(ColoredLevelConfig::default())
+        .level_for("tauri", log::LevelFilter::Warn)
+        .level_for("wry", log::LevelFilter::Warn)
+        .level_for("tracing", log::LevelFilter::Warn)
+        .level_for("cortex_lib", log::LevelFilter::Info)
+        .split(app.handle())?;
+
+    // on debug builds, set up the DevTools plugin and pipe the logger from tauri-plugin-log
+    #[cfg(debug_assertions)]
+    {
+        let mut devtools_builder = tauri_plugin_devtools::Builder::default();
+        devtools_builder.attach_logger(logger);
+        app.handle().plugin(devtools_builder.init())?;
+    }
+    // on release builds, only attach the logger from tauri-plugin-log
+    #[cfg(not(debug_assertions))]
+    {
+        tauri_plugin_log::attach_logger(_max_level, logger);
+    }
+
+    app.handle().plugin(tauri_plugin_log)?;
+
+    Ok(())
+}
+
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_decorum::init())
+        .setup(|app| {
+            setup_logger(app)?;
+            // Create a custom titlebar for main window
+            // On Windows this will hide decoration and render custom window controls
+            // On macOS it expects a hiddenTitle: true and titleBarStyle: overlay
+            let main_window = app.get_webview_window("main").unwrap();
+            main_window.create_overlay_titlebar().unwrap();
+
+            #[cfg(target_os = "macos")]
+            main_window.set_traffic_lights_inset(16.0, 20.0).unwrap();
+            
+            Ok(())
+        });
+    builder
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_window_state::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
